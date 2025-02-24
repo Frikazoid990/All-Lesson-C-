89b6b5db84ef255b38580c14b94945af4675a094
Frikazoid <microsoft@frikazoid.ru>
2025-02-24 18:57:58 +0300

    1

---
+++ MT OOP 01/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT_OOP_01
+{
+    public class Person
+    {
+        public string name { get; set; }
+
+        
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
---
+++ MT OOP 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT_OOP_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Bob", "Microsoft");
+
+            //employee1.Print();
+
+            Person person1 = employee1;
+
+            person1.Print();
+
+        }
+    }
+}
