105a119c90460eb19dde2947daf14e5c982b83dc
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-02-26 15:44:28 +0300

    1

---
+++ MT 06 Find block catch when handling exception/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT_06_Find_block_catch_when_handling_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("=============");
+                Console.WriteLine($"Catch в Main : {ex.Message}");
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine();
+                Console.WriteLine(ex.TargetSite);
+                Console.WriteLine("=============");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("Конец метода Main");
+        }
+    }
+}
