b06ea71055ee94734aca2af2e845772158998e25
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-02-26 23:57:45 +0300

    1

--- Ch6 MT 01.01 Using delegates/Program.cs
+++ Ch6 MT 01.01 Using delegates/Program.cs
@@ -11,7 +11,16 @@ namespace Ch6_MT_01._01_Using_delegates
     {
         static void Main(string[] args)
         {
+            // создаем банковский счет
+            Account account = new Account(200);
+            // Добавляем в делегат ссылку на метод PrintSimpleMessage
+            account.RegisterHandler(PrintSimpleMessage);
+            // Два раза подряд пытаемся снять деньги
+            account.Take(100);
+            account.Take(150);
+
             
         }
+        static void PrintSimpleMessage(string message) => Console.WriteLine(message);
     }
 }
