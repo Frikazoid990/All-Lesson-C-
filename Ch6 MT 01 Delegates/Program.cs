1aa7b142c7f1ac7fcb2e4e380bad892cd81adf6e
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-02-26 15:56:28 +0300

    1

---
+++ Ch6 MT 01 Delegates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_MT_01_Delegates
+{
+    class Program
+    {
+
+        delegate void Message(string m);
+
+        static void Hello(string message) => Console.WriteLine($"{message}");
+
+        static void Main(string[] args)
+        {
+            Message mes;
+            mes = Hello;
+
+            mes("22");
+            
+        }
+
+
+    }
+}
