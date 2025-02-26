28c98a51638ccdf597ab4b61a75cf7c94756032c
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-02-26 13:47:56 +0300

    1

---
+++ MT OOP 03.02 Generic/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT_OOP_03._02_Generic
+{
+    public class Person<T>
+    {
+        public T Id { get; }
+
+        public Person(T Id)
+        {
+            this.Id = Id;
+        }
+    }
+
+    public class UniversalPerson<T> : Person<T>
+    {
+        public UniversalPerson(T Id) : base(Id)
+        {
+        }
+    }
+
+    public class StringPerson : Person<string>
+    {
+        public StringPerson(string Id) : base(Id)
+        {
+        }
+    }
+
+    public class IntPerson<T> : Person<int>
+    {
+        public T code { get; set; }
+        public IntPerson(int Id, T code) : base(Id)
+        {
+            this.code = code;
+        }
+    }
+}
--- MT OOP 03.02 Generic/Program.cs
+++ MT OOP 03.02 Generic/Program.cs
@@ -10,6 +10,28 @@ namespace MT_OOP_03._02_Generic
     {
         static void Main(string[] args)
         {
+            Person<string> person1 = new Person<string>("31");
+
+            Person<int> person2 = new UniversalPerson<int>(32);
+
+            UniversalPerson<int> person3 = new UniversalPerson<int>(33);
+
+            StringPerson person4 = new StringPerson("438767");
+
+            Person<string> person5 = new StringPerson("43875");
+            // так нельзя написать
+            //Person<int> person6 = new StringPerson("45545");
+            IntPerson<string> person7 = new IntPerson<string>(123, "ab23");
+            Person<int> person8 = new IntPerson<long>(7, 42141242);
+            var person9 = (IntPerson<long>)person8;
+            person9.code = 102;
+            Console.WriteLine(person9.code);
+            var person10 = (IntPerson<long>)person8;
+            Console.WriteLine(person10.code);
+            Console.WriteLine(person1.Id);
+            Console.WriteLine(person2.Id);
+            Console.WriteLine(person3.Id);
+            
         }
     }
 }
