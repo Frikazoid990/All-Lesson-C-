7c4ab1d95330cb641e7f4b10d1ddf120417fc7b2
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-02-28 00:19:27 +0300

    1

---
+++ Ch6 MT 02 Anonim methods/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_MT_02_Anonim_methods
+{
+    class Program
+    {
+        delegate void MessageHandler(string message);
+
+        delegate int Operation(int x, int y);
+
+        static void ShowMessage(string message, MessageHandler handler)
+        {
+            handler(message);
+        }
+
+        static void Main(string[] args)
+        {
+            MessageHandler handler = delegate (string mes)
+            {
+                Console.WriteLine(mes);
+            };
+
+            handler("What`s up?");
+
+
+            ShowMessage("Hello!...", handler);
+
+            ShowMessage("hello!", delegate (string mes)
+            {
+                Console.WriteLine(mes);
+            });
+
+            Operation operation = delegate (int x, int y)
+            {
+                return x + y;
+            };
+
+            int z = operation(2, 1);
+
+            Console.WriteLine(z);
+
+            Operation operation1 = delegate (int x, int y)
+            {
+                return x + y + z;
+            };
+
+            int result = operation1(2, 1);
+            Console.WriteLine(result);
+
+        }
+    }
+}
