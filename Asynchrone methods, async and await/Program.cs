72508a71f017fb371ea4c3fec67ef9a50155fac0
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-04-13 18:36:19 +0300

    1

---
+++ Asynchrone methods, async and await/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynchrone_methods__async_and_await
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await PrintAsync();
+
+            var tomTask = PrintNameAsync("Tom");
+            var bobTask = PrintNameAsync("Bob");
+            var samTask = PrintNameAsync("Sam");
+
+            await tomTask;
+            await bobTask;
+            await samTask;
+            // определение асинхронного метода
+
+
+        }
+
+        static async Task PrintNameAsync(string name)
+        {
+            await Task.Delay(3000);     // имитация продолжительной работы
+            Console.WriteLine(name);
+        }
+
+        static void Print()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Hello");
+        }
+
+        static async Task PrintAsync()
+        {
+            Console.WriteLine("Начало асинхронного метода");
+            await Task.Run(Print);
+            Console.WriteLine("Конец асинхронного метода");
+        }
+    }
+}
--- Test1/Program.cs
+++ Test1/Program.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Test1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
+            string input = "2 неделя Аксус ИнтЕрНет-Продажи.xlsx";
+            input.Split(".");
+            input = input.ToUpper();
+            if (input.Contains("ИНТЕРНЕТ"))
+                Console.WriteLine("yes");
+
+
+
+
+            
         }
     }
 }
