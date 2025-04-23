69a6e6cd6e069de8cf17fd0ec8c692eb4e5691e0
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-04-23 08:31:39 +0300

    Events

---
+++ Ch6 MT 04 Events/Class/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events.Class
+{
+    
+
+    public class Account
+    {
+        public delegate void AccountHandler(string message);
+
+        public event AccountHandler? Notify;
+
+        public int sum { get; private set; }
+
+        public Account(int sum) { this.sum = sum; }
+
+        public void Put(int sum)
+        {
+            this.sum += sum;
+            Notify?.Invoke($"На счет поступило: {sum}");
+        }
+
+        public void Take(int sum)
+        {
+            if (this.sum >= sum)
+            {
+                this.sum -= sum;
+                Notify?.Invoke($"Со счета снято: {sum}");   // 2.Вызов события
+            }
+            else
+            {
+                Notify?.Invoke($"Недостаточно денег на счете. Текущий баланс: {this.sum}"); ;
+            }
+        }
+    }
+}
--- Ch6 MT 04 Events/Program.cs
+++ Ch6 MT 04 Events/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Events.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,22 @@ namespace Events
     {
         static void Main(string[] args)
         {
+            Account account = new Account(100);
+            account.Notify += DisplayMessage;
+            account.Notify += DisplayMessage1;
+            account.Put(20);
+            account.Take(70);
+            account.Take(180);
+            
 
+        }
+
+        private static void DisplayMessage(string message) => Console.WriteLine(message);
+
+        private static void DisplayMessage1(string message)
+        {
+            message = "Опа должен был косарь отдать!";
+            Console.WriteLine(message);
         }
     }
 }
