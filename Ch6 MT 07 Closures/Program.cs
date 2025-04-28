4fe186711404c3bc2170c08e1a03c1ab4325a685
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-04-28 16:54:19 +0300

    1

---
+++ Ch6 MT 07 Closures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_MT_07_Closures
+{
+    internal class Program
+    {
+        static Action Outer()
+        {
+            int x = 5;
+            void Inner()
+            {
+                x++;
+                Console.WriteLine(x);
+            }
+            return Inner;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            var fn = Outer(); // fn = Inner, так как метод Outer возвращает функцию Inner
+            //Вызываем внутреннюю функцию Inner
+            fn();
+            fn();
+            fn();
+
+            var outerFn = () =>
+            {
+                int x = 10;
+                var innerFn = () => Console.WriteLine(++x);
+                return innerFn;
+            };
+
+            var fn1 = outerFn();
+            fn1();
+            fn1();
+            fn1();
+        }
+
+
+    }
+}
---
+++ Defining interfaces/IMovable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Defining_interfaces
+{
+    internal interface IMovable
+    {
+        const int minSpeed = 0;     //Доступно с версией C# 8.0
+
+        static int maxSpeed = 60;   //Доступно с версией C# 8.0
+
+        void Move();                //Движение
+
+        string Name { get; set; }
+
+        delegate void MoveHandler(string message); // определение делага для события
+
+        event MoveHandler MoveEvent; // событие движения
+    }
+}
