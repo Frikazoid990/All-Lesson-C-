5536266de8299da179b981c5771e5398d5c69b42
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-04-28 13:56:47 +0300

    1

---
+++ Ch6 MT 06 Delegates Action Func and Predicate/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_MT_06_Delegates_Action_Func_and_Predicate
+{
+    internal class Program
+    {
+        //Action
+        static void DoOperation(int a, int b, Action<int,int> op) => op(a,b);
+
+        static void Add(int x, int y) => Console.WriteLine($"{x} + {y} = {x + y}");
+
+        static void Multiply(int x , int y) => Console.WriteLine($"{x} * {y} = {x * y}");
+        //Action
+
+        //Predicate
+
+        //Predicate
+        static void Main(string[] args)
+        {
+            //Action
+            DoOperation(10, 6, Add);
+            DoOperation(10, 6, Multiply);
+            //Predicate
+
+            
+        }
+    }
+}
---
+++ Ch6 MT 06 Delegates Action Func and Predicate/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Ch6 MT 06 Delegates Action Func and Predicate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ch6 MT 06 Delegates Action Func and Predicate")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0d7e3f31-1581-4d6c-9c0a-8b7dd2a9283c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
