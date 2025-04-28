bd8eff1fffdee7bb21fce55d96637824d773abf6
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2025-04-28 14:47:39 +0300

    1

--- Ch6 MT 06 Delegates Action Func and Predicate/Program.cs
+++ Ch6 MT 06 Delegates Action Func and Predicate/Program.cs
@@ -16,17 +16,34 @@ namespace Ch6_MT_06_Delegates_Action_Func_and_Predicate
         static void Multiply(int x , int y) => Console.WriteLine($"{x} * {y} = {x * y}");
         //Action
 
-        //Predicate
+        //Func
 
-        //Predicate
+        static int DoOperation1(int n, Func<int, int> op) => op(n);
+
+        static int DoubleNumber(int n) => 2 * n;
+
+        static int SquareNumber(int n) => n * n;
+
+        //Func
+
         static void Main(string[] args)
         {
             //Action
             DoOperation(10, 6, Add);
             DoOperation(10, 6, Multiply);
             //Predicate
+            Predicate<int> isPositive = (int x) => x > 0;
+            Console.WriteLine(isPositive(20));
+            Console.WriteLine(isPositive(30));
+            //Func
+            int result1 = DoOperation1(6, DoubleNumber);
+            Console.WriteLine(result1);
 
-            
+            int result2 = DoOperation1(6, SquareNumber);
+            Console.WriteLine(result2);
+
+
+
         }
     }
 }
