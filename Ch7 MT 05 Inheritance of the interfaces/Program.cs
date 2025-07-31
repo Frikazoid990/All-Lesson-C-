217c01e1950bbd0aef8bba7f7ba8497dde61a409
Frikazoid990 <skripkindan0812@gmail.com>
2025-07-31 11:52:37 +0300

    Ch7 MT 05 add some code

---
+++ Ch7 MT 05 Inheritance of the interfaces/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ch7_MT_05_Inheritance_of_the_interfaces;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAction act1 = new RunActionExample1();
+        act1.Move();
+        
+        IRunAction act2 = new RunActionExample1();
+        act2.Move();
+        Console.WriteLine("------------------------------------");
+        IAction act3 = new RunActionExample2();
+        act3.Move();
+        IRunAction act4 = new RunActionExample2();
+        act4.Move();
+        RunActionExample2 act5 = new RunActionExample2();
+        act5.Move();
+
+    }
+
+}
+
+
+interface IAction
+{
+    void Move() => Console.WriteLine("I`m moving");
+}
+
+interface IRunAction: IAction
+{
+    new void Move() => Console.WriteLine("I`m running");
+}
+
+public class RunActionExample1 : IRunAction
+{
+        
+}
+
+public class RunActionExample2 : IRunAction
+{
+    public void Move() => Console.WriteLine("I`m tired");
+}
