abd2ddf0cb1e984c3ad6a0859d4f1e000ae1e983
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2026-03-12 00:23:51 +0300

    1

--- MT 01 Multithreading and class Thread/Program.cs
+++ MT 01 Multithreading and class Thread/Program.cs
@@ -1,5 +1,4 @@
 ﻿namespace CH_14_MT_01_Multithreading_and_class_Thread;
-
 class Program
 {
     static void Main(string[] args)
---
+++ MT 03 Threads with params/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MT_03_Threads_with_params;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread thread1 = new Thread(new ParameterizedThreadStart(Print));
+        Thread thread2 = new Thread(Print);
+        Thread thread3 = new Thread(message => Console.WriteLine("{0}", message));
+        thread1.Start(1);
+        thread2.Start(2);
+        thread3.Start("Hello");
+    }
+
+    static void Print(object param)
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine("{2}  Поток номер: {0}: {1}",param, i, DateTime.Now);
+            Thread.Sleep(500);
+            
+        }
+    }
+}
