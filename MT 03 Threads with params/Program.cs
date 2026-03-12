e84d36f9d9255e2fb441fd14193030b750858f4a
senichkin.aa <senichkin.aa@glonasssoft.ru>
2026-03-12 23:16:34 +0300

    1

--- MT 03 Threads with params/Program.cs
+++ MT 03 Threads with params/Program.cs
@@ -4,12 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Thread thread1 = new Thread(new ParameterizedThreadStart(Print));
-        Thread thread2 = new Thread(Print);
-        Thread thread3 = new Thread(message => Console.WriteLine("{0}", message));
-        thread1.Start(1);
-        thread2.Start(2);
-        thread3.Start("Hello");
+        // Thread thread1 = new Thread(new ParameterizedThreadStart(Print));
+        // Thread thread2 = new Thread(Print);
+        // Thread thread3 = new Thread(message => Console.WriteLine("{0}", message));
+        // thread1.Start(1);
+        // thread2.Start(2);
+        // thread3.Start("Hello");
+        //
+
+
+        var number = 4;
+        Thread thread4 = new Thread(new ParameterizedThreadStart(Print1));
+        thread4.Start(number);
+        var tom = new Person("Tom", 24);
+        Thread thread5 = new Thread(new ParameterizedThreadStart(Print1));
+        thread5.Start(tom);
     }
 
     static void Print(object param)
@@ -21,4 +30,32 @@ class Program
             
         }
     }
+
+    static void Print1(object param)
+    {
+        if (param is int n)
+        {
+            Console.WriteLine($"{DateTime.Now}: n * n: {n * n}");
+        }
+
+        if (param is Person person)
+        {
+            Console.WriteLine($"{DateTime.Now}:\n" +
+                              $"Name:{person.Name}\n" +
+                              $"Age:{person.Age}");
+        }
+    }
+}
+
+public class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    
+    public string Name { get; set; }
+    
+    public int Age { get; set; }
 }
---
+++ MT 04 Thread synchronization/Program.cs
@@ -0,0 +1,48 @@
+﻿var x = 0;
+
+
+
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(Print);
+    myThread.Name = $"Поток {i}";   // устанавливаем имя для каждого потока
+    myThread.Start();
+}
+
+Thread.Sleep(1000);
+Console.WriteLine(x);
+x = 0;
+var locker = new Object();
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new(PrintWithLock);
+    myThread.Name = $"Поток {i}";   // устанавливаем имя для каждого потока
+    myThread.Start();
+}
+
+void Print()
+{
+    x = 1;
+    for (int i = 1; i < 6; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+        x++;
+        Thread.Sleep(100);
+    }
+}
+
+void PrintWithLock()
+{
+
+    lock (locker)
+    {
+        x = 1;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(100);
+        }
+    }
+
+}
