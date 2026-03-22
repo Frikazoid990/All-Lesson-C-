6114b239d7d78330a67d9e4bd3020e8420accfb7
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2026-03-23 01:02:40 +0300

    Add MT 06 Lock class and synchronization

--- MT 05 Monitors/Program.cs
+++ MT 05 Monitors/Program.cs
@@ -2,8 +2,54 @@
 
 class Program
 {
+    static int x = 0;
+    static object locker = new();  // объект-заглушка
     static void Main(string[] args)
     {
-        
+
+        // запускаем пять потоков
+        for (int i = 1; i < 6; i++)
+        {
+            Thread myThread = new(PrintWithKeywordLock);
+            myThread.Name = $"Поток {i}";
+            myThread.Start();
+        }
+    }
+
+    static void PrintWithKeywordLock()
+    {
+        lock (locker)
+        {
+            x = 1;
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                x++;
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    static void PrintWithMonitorLock()
+    {
+        bool acquiredLock = false;
+        try
+        {
+            Monitor.Enter(locker, ref acquiredLock);
+            x = 1;
+            for (int i = 1; i < 6; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+                x++;
+                Thread.Sleep(100);
+            }
+        }
+        finally
+        {
+            if (acquiredLock)
+            {
+                Monitor.Exit(locker);
+            }
+        }
     }
 }
