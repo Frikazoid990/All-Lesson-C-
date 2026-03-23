cb0528e5184823e4587c88e011cbea3c437d21e2
senichkin.aa <senichkin.aa@glonasssoft.ru>
2026-03-23 17:56:35 +0300

    idei

--- MT 06 Lock class and synchronization/Program.cs
+++ MT 06 Lock class and synchronization/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿int x = 0; //некоторый общий ресурс
+Lock _locker = new();
+
+for (int i = 1; i < 6; i++)
+{
+    Thread myThread = new Thread(PrintWithKeyWordlock);
+    myThread.Name = $"Поток {i}";
+    myThread.Start();
+}
+
+void PrintWithKeyWordlock()
+{
+    lock (_locker)
+    {
+        x = 1;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            x++;
+            Thread.Sleep(150);
+        }    
+    }
+    
+}
