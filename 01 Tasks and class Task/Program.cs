356fcfebdf87ea49b657ad04238d80ee8b023609
Frikazoid990 <89697961+Frikazoid990@users.noreply.github.com>
2026-05-20 20:57:08 +0300

    1

---
+++ 01 Tasks and class Task/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_Tasks_and_class_Task;
+
+class Program
+{
+    delegate void Message(); // 1. Объявляем делегат
+    static void Hello() => Console.WriteLine("Hello METANIT.COM");
+    
+    static void Main(string[] args)
+    {
+        Message mes;            // 2. Создаем переменную делегата
+        mes = Hello;            // 3. Присваиваем этой переменной адрес метода
+        mes();                  // 4. Вызываем метод
+        
+    }
+}
